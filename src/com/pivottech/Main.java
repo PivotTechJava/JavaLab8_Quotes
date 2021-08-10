c569ec5cf91160facb84d8dbc7a64a3c76776ad5
Shaun Carter <why06mail@gmail.com>
2021-08-10 14:24:31 -0500

    feat: moved .gitignore to root feat: added java args example

--- src/com/pivottech/Main.java
+++ src/com/pivottech/Main.java
@@ -1,8 +1,19 @@
 package com.pivottech;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Arguments
+        System.out.println(Arrays.toString(args));
+        /*
+         * args[0] would be command args[1] would be the input
+         * Ex: author Chimamanda
+         * args[0] is "author" | args[1] is "Chimamanda"
+         */
+
+	    // Good luck!
+
     }
 }
